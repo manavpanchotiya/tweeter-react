fc86bddda3c7e883cd9611f8c72939a78126e686
manavpanchotiya <manavpanchotiya@gmail.com>
2025-05-12 21:30:19 -0500

    redesigned with react

---
+++ react-version/src/components/Profile.jsx
@@ -0,0 +1,15 @@
+function Navigation() {
+  return (
+    <aside>
+    <div className="profile">
+      <img className="profile__image" src="./public/profile-hex.png"/>
+    </div>
+    <br/>
+    <div className="profile__name">
+      <h2><span className="profile--bold">Amy</span> Mansell</h2>
+    </div>
+  </aside>
+  );
+}
+
+export default Navigation;
