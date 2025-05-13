8ac3e5ec5d42eab5404b183e3845aec12b0eb2fa
manavpanchotiya <manavpanchotiya@gmail.com>
2025-05-12 22:01:28 -0500

    redesigned with react

--- react-version/src/components/Profile.jsx
+++ react-version/src/components/Profile.jsx
@@ -1,15 +1,19 @@
-function Navigation() {
-  return (
+function Profile() {
+  const firstName = "Amy"
+  const lastName = "Mansell"
+  const avatar = "./public/profile-hex.png"
+
+  return(
     <aside>
     <div className="profile">
-      <img className="profile__image" src="./public/profile-hex.png"/>
+      <img className="profile__image" src={avatar}/>
     </div>
     <br/>
     <div className="profile__name">
-      <h2><span className="profile--bold">Amy</span> Mansell</h2>
+      <h2><span className="profile--bold">{firstName}</span> {lastName}</h2>
     </div>
   </aside>
-  );
+  )
 }
 
-export default Navigation;
+export default Profile;
--- react-version/src/components/TweetForm.jsx
+++ react-version/src/components/TweetForm.jsx
@@ -1,8 +1,10 @@
 function TweetForm() {
+  const textArea = "What's on your mind?"
+
   return (
     <section className="newtweet">
       <form method="post" action="/tweets" className="newtweet__form">
-        <textarea className="form__textarea" name="text" placeholder="What are you humming about?"></textarea>
+        <textarea className="form__textarea" name="text" placeholder={textArea}></textarea>
         <input type="submit" value="Tweet" className="form__input"/>
         <span className="form__counter">140</span>
       </form>
